f194b60179cb36c456e140b073bc784e20c22bd8
Rishabh Sharma <158538627+Crazzyman1081@users.noreply.github.com>
2026-02-05 06:01:16 +0530

    Force base path on GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS === 'true';
 const repoName = 'My-Blogs'; // Your repository name
 
 const nextConfig: NextConfig = {
